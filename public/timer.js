9b18c1d9f136f459a2b0750b0e2865434dfbe251
Dennis Kupec <denniskupec@gmail.com>
2016-10-09 18:38:37 -0500

    added a way to inspect timer drift

--- public/timer.js
+++ public/timer.js
@@ -1,18 +1,38 @@
+/* 
+epoch.link | rev. 2
+(c) 2016, Dennis Kupec
+MIT License
+*/
+
 (function() {
 
+	var i = 0;
 	var holder = document.getElementById('thetime');
 
 	var setTimestamp = function() {
+		i += 1;
 		holder.innerText = Math.floor(Date.now() / 1000);
-		document.title = "epoch.link | " + holder.innerText;
+		document.title = "epoch.link | " + holder.innerText; 
 	};
 
 	setTimestamp();
 
-	/* Not like this has to be accurate to the millisecond... */
+	setInterval(setTimestamp, 1000);
 
-	var interval = setInterval(setTimestamp, 990);
+	// index.html#delta<m> 
+	// where m is minutes to wait between timer drift checks
+	if(window.location.hash.includes("#delta")) {
+		var m = parseFloat(window.location.hash.replace("#delta", ""));
+			m = isNaN(m) ? 1 : m;
 
-	/* Every time you shim for an old browser, a whale is hunted. */
+		console.info("drift check interval: " + m*1000*60);
+
+		setInterval(function(){
+			var d = Math.floor(Date.now() / 1000);
+			var e = parseInt(holder.innerText);
+
+			console.info({ i: i, now: d, el: e, delta: (d-e) });
+		}, 1000*60*m);
+	}
 
 })();
