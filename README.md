e5da43a5a385982620a1c8b57a2eccb1310db5c7
Dennis Kupec <denniskupec@gmail.com>
2016-10-12 18:24:15 -0500

    delta timer removal, revert to more accurate timer

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # epoch.link
 
 [Had to do something with this domain.](https://twitter.com/iamdevloper/status/748871909559799808)
-
-
-https://epoch.link/index.html#delta3  
-Every ~3 minutes the drift between the interval updating `#thetime` and `Date.now()` is output in the console.  
-Swap out the number for a different polling interval.
--- public/timer.js
+++ public/timer.js
@@ -6,34 +6,16 @@ MIT License
 
 (function() {
 
-	var i = 0;
 	var holder = document.getElementById('thetime');
 
 	var setTimestamp = function() {
-		i += 1;
-		//holder.innerText = Math.floor(Date.now() / 1000);
-		holder.innerText = parseInt(holder.innerText) + 1;
+		holder.innerText = Math.floor(Date.now() / 1000);
+		//holder.innerText = parseInt(holder.innerText) + 1;
 		document.title = "epoch.link | " + holder.innerText; 
 	};
 
-	holder.innerText = Math.floor(Date.now() / 1000);
+	setTimestamp();
 
 	setInterval(setTimestamp, 1000);
-
-	// index.html#delta<m> 
-	// where m is minutes to wait between timer drift checks
-	if(window.location.hash.includes("#delta")) {
-		var m = parseFloat(window.location.hash.replace("#delta", ""));
-			m = isNaN(m) ? 1 : m;
-
-		console.info("drift check interval: " + m*1000*60);
-
-		setInterval(function(){
-			var d = Math.floor(Date.now() / 1000);
-			var e = parseInt(holder.innerText);
-
-			console.info({ i: i, now: d, el: e, delta: (d-e) });
-		}, 1000*60*m);
-	}
 
 })();
